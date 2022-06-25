50b54d4efb789c12717ba73ae6b2e134074807c9
Sneha <sneha.k2019@vitstudent.ac.in>
2022-06-25 23:48:09 +0530

    changes updated

--- Online Examination System/adminpanel/admin/pages/home.php
+++ Online Examination System/adminpanel/admin/pages/home.php
@@ -6,14 +6,8 @@
             <div class="app-page-title">
                 <div class="page-title-wrapper">
                     <div class="page-title-heading">
-                        <div class="page-title-icon">
-                            <i class="pe-7s-car icon-gradient bg-mean-fruit">
-                            </i>
-                        </div>
-                        <div>Analytics Dashboard
-                            <div class="page-title-subheading">This is an example dashboard created using build-in elements and components.
-                            </div>
-                        </div>
+                        
+                        
                     </div>
                     <div class="page-title-actions">
                         <button type="button" data-toggle="tooltip" title="Example Tooltip" data-placement="bottom" class="btn-shadow mr-3 btn btn-dark">
@@ -127,51 +121,10 @@
             </div>
     
             <div class="row">
-                <div class="col-md-6 col-xl-4">
-                    <div class="card mb-3 widget-content">
-                        <div class="widget-content-outer">
-                            <div class="widget-content-wrapper">
-                                <div class="widget-content-left">
-                                    <div class="widget-heading">Total Orders</div>
-                                    <div class="widget-subheading">Last year expenses</div>
-                                </div>
-                                <div class="widget-content-right">
-                                    <div class="widget-numbers text-success">1896</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                
                 </div>
-                <div class="col-md-6 col-xl-4">
-                    <div class="card mb-3 widget-content">
-                        <div class="widget-content-outer">
-                            <div class="widget-content-wrapper">
-                                <div class="widget-content-left">
-                                    <div class="widget-heading">Products Sold</div>
-                                    <div class="widget-subheading">Revenue streams</div>
-                                </div>
-                                <div class="widget-content-right">
-                                    <div class="widget-numbers text-warning">$3M</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6 col-xl-4">
-                    <div class="card mb-3 widget-content">
-                        <div class="widget-content-outer">
-                            <div class="widget-content-wrapper">
-                                <div class="widget-content-left">
-                                    <div class="widget-heading">Followers</div>
-                                    <div class="widget-subheading">People Interested</div>
-                                </div>
-                                <div class="widget-content-right">
-                                    <div class="widget-numbers text-danger">45,9%</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                
+               
                 <div class="d-xl-none d-lg-block col-md-6 col-xl-4">
                     <div class="card mb-3 widget-content">
                         <div class="widget-content-outer">
